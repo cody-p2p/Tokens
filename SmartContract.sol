b656f88ac2fac69a9e93727a7ff9adac6a92d30a
Cody <162437900+cody-p2p@users.noreply.github.com>
2024-09-02 14:55:21 -0700

    Update SmartContract.sol
    DATA Index connect select mapper

--- SmartContract.sol
+++ SmartContract.sol
@@ -3,19 +3,36 @@ pragma solidity ^0.8.0;
 contract SimpleContract {
     address public owner;
     mapping (address => uint) public balances;
+    mapping (address => bool) public isFrozen;
 
     constructor() public {
         owner = msg.sender;
         balances[owner] = 1000;
+        isFrozen[owner] = false;
     }
 
     function transfer(address recipient, uint amount) public {
         require(balances[msg.sender] >= amount, "Insufficient balance");
+        require(!isFrozen[msg.sender], "Account is frozen");
         balances[msg.sender] -= amount;
         balances[recipient] += amount;
     }
 
+    function freezeAccount(address account) public {
+        require(msg.sender == owner, "Only the owner can freeze an account");
+        isFrozen[account] = true;
+    }
+
+    function unfreezeAccount(address account) public {
+        require(msg.sender == owner, "Only the owner can unfreeze an account");
+        isFrozen[account] = false;
+    }
+
     function getBalance(address account) public view returns (uint) {
         return balances[account];
+    }
+
+    function getFrozenStatus(address account) public view returns (bool) {
+        return isFrozen[account];
     }
 }
